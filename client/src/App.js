3e2339bcd442b7d02dfe3a21b4d3d0fdb191292a
NoorZak <n.zakarneh1@student.aaup.edu>
2021-02-17 15:41:37 +0200

    aaa

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-import {Router} from '@reach/router';
-import Form from './components/Form';
-import Main from './views/Main';
-import Details from './components/Details';
-import Update from './views/Update';
+import {Router, Redirect,Link} from "@reach/router";
+
+//View imports
+import ManagePlayers from "./views/ManagePlayers.jsx";
+import ManageStatus from "./views/ManageStatus.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Main path="products/"/>
-        <Details path="products/:id" /> 
-        <Update path="products/:id/edit"/>
+    <div className="App" >
+      <nav>
+        <Link
+            to="/players/list"
+        >Manage Players</Link>
+        <h1>Team Manager</h1>
+        <Link
+            to="/status"
+        >Manage Player Status</Link>
+      </nav>
       
-      </Router> 
-      </div>
+      <Router>
+        <ManagePlayers path="/players/*"/>
+        <ManageStatus path="/status/*"/>
+        <Redirect noThrow from="/" to="/players"/>
+      </Router>
+    </div>
   );
 }
 
